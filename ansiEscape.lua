b31daabb29065f02741391f327a1e500306e3f82
r08ust <jjq0809@sina.com>
2021-06-03 11:20:35 +0800

    fix base64

--- ansiEscape.lua
+++ ansiEscape.lua
@@ -1,4 +1,4 @@
--- require('base64')
+require('base64')
 ansiEscape = {}
 local BEL = '\u{007}'
 local BS = '\u{008}'
--- base64.lua
+++ base64.lua
@@ -39,54 +39,58 @@ function base64.encode(str)
 		error("integer overflow")
 	end
 	local retVal = ''
-	local ends = len * 2
+	local ends = len 
 	local block = str
+    local index = 0
 	local line_len = 0
-	while ends - block:len() >= 3 do
-    	local blockChar0 = ord(block:sub(1, 1))
-		local blockChar1 = ord(block:sub(2, 2))
-		local blockChar2 = ord(block:sub(3, 3))
-		local charSetPos = bit.rshift(blockChar0, 2)
+	while ends - index >= 3 do
+    	local blockChar0 = (block:sub(index + 1, index + 1)):byte()
+		local blockChar1 = (block:sub(index + 2, index + 2)):byte()
+		local blockChar2 = (block:sub(index + 3, index + 3)):byte()
+		local charSetPos = bit.rshift(blockChar0, 2) + 1
 		retVal = retVal .. charSet:sub(charSetPos, charSetPos)
-		charSetPos = bit.bor(bit.lshift(bit.band(blockChar0, 0x03), 4), bit.rshift(blockChar1, 4))
-		retVal = retVal .. charSet:sub(charSetPos, charSetPos)
-		charSetPos = bit.bor(bit.lshift(bit.band(blockChar1, 0x0f), 2), bit.rshift(blockChar2, 6))
-		retVal = retVal .. charSet:sub(charSetPos, charSetPos)
-		charSetPos = bit.band(blockChar2, 0x3f)
-		retVal = retVal .. charSet:sub(charSetPos, charSetPos)
-		block = block:sub(4)
+		charSetPos = bit.bor(bit.lshift(bit.band(blockChar0, 0x03), 4), bit.rshift(blockChar1, 4)) + 1
+		retVal = retVal .. charSet:sub(charSetPos, charSetPos) 
+		charSetPos = bit.bor(bit.lshift(bit.band(blockChar1, 0x0f), 2), bit.rshift(blockChar2, 6)) + 1
+		retVal = retVal .. charSet:sub(charSetPos, charSetPos) 
+		charSetPos = bit.band(blockChar2, 0x3f) + 1
+		retVal = retVal .. charSet:sub(charSetPos, charSetPos) 
+		-- block = block:sub(4)
+        index = index + 3
 		line_len = line_len + 4
 		if line_len >= 72 then
 			retVal = retVal .. '\n'
 			line_len = 0
 		end
 	end
-	print(1)
-	if (ends - block) then
+	if (ends - index) ~= 0 then
 		retVal = retVal
-    	local blockChar0 = ord(block:sub(1, 1))
-		local blockChar1 = ord(block:sub(2, 2))
-		local blockChar2 = ord(block:sub(3, 3))
+    	local blockChar0 = (block:sub(index + 1, index + 1)):byte()
+		local blockChar1 = (block:sub(index + 2, index + 2)):byte()
+		local blockChar2 = (block:sub(index + 3, index + 3)):byte()
 		local charSetPos = bit.rshift(blockChar0, 2)
-		if (ends - block == 1) then
+		if (ends - index == 1) then
 			charSetPos = bit.band(blockChar0, 0x3)
-			charSetPos = bit.lshift(charSetPos, 4)
+			charSetPos = bit.lshift(charSetPos, 4) + 1
 			retVal = retVal .. charSet:sub(charSetPos, charSetPos)
 			retVal = retVal .. '='
 		else 
 			charSetPos = bit.band(blockChar0, 0x3)
 			charSetPos = bit.lshift(charSetPos, 4)
-			charSetPos = bit.bor(charSetPos, bit.rshift(blockChar1, 4))
+			charSetPos = bit.bor(charSetPos, bit.rshift(blockChar1, 4)) + 1
 			retVal = retVal .. charSet:sub(charSetPos, charSetPos)
-			charSetPos = bit.lshift(bit.band(blockChar1, 0xf), 2)
+			charSetPos = bit.lshift(bit.band(blockChar1, 0xf), 2) + 1
 			retVal = retVal .. charSet:sub(charSetPos, charSetPos)
 		retVal = retVal .. '='
 		line_len = line_len + 4
 	    end
     end
-	if line_len then
+	--[[ if line_len ~= 0  then
 		retVal = retVal .. '\n'
 	end
-	-- retVal = retVal .. '\0'
+	retVal = retVal .. '\0' ]]
 	return retVal
 end
+
+--[[ local res = (base64.encode('lua base64'))
+print(res) ]]
